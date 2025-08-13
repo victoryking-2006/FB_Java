605abc57fd9a603dc7f14c35bf77dc9c5727f057
victoryking-2006 <skp272534@gmail.com>
2025-08-13 09:19:45 +0530

    Add files via upload
    added my first program of java

---
+++ first.java
@@ -0,0 +1,9 @@
+public class first{
+    public static void main(String[] args)
+    
+    {
+        System.out.println("britansh");
+        System.out.println("btech_cse");
+        System.out.println("2415000463");
+    }
+}
