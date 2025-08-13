d93562a9fe9495084f70dff0a76ffa4007e0fa46
victoryking-2006 <skp272534@gmail.com>
2025-08-13 10:17:17 +0530

    Add files via upload
    i learned about types of  variables and its uses

---
+++ variable.java
@@ -0,0 +1,10 @@
+public class variable {
+    static int a=2;
+    public static void main(String[] args){
+        int b=6;
+        System.out.println(b);
+        System.out.println(variable.a);
+
+    }
+    
+}
