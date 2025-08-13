92a87d529fe413dcfc5ce107ef18a510f7ae903c
victoryking-2006 <skp272534@gmail.com>
2025-08-13 14:36:09 +0530

    Add files via upload
    in  this we  learned about datatype their sizes and ranges

---
+++ dtype.java
@@ -0,0 +1,38 @@
+// premative data type-:
+// byte,int,float,char,boolean,short,long,double
+// size-: 4    2    2      1bit   2     8     8
+
+// public class dtype {
+//     public static void main(String[] args)
+//     {
+//         System.out.println(Byte.MIN_VALUE + " to " + Byte.MAX_VALUE);
+//         System.out.println(Integer.MIN_VALUE + " to " + Integer.MAX_VALUE);
+//         System.out.println(Character.MIN_VALUE + " to " + Character.MAX_VALUE);
+//         System.out.println(Float.MIN_VALUE + " to " + Float.MAX_VALUE);
+//         System.out.println(Short.MIN_VALUE + " to " + Short.MAX_VALUE);
+//         System.out.println(Boolean.MIN_VALUE + "to" + Boolean.MAX_VALUE);
+//         System.out.println(Long.MIN_VALUE + " to " + Long.MAX_VALUE);
+//         System.out.println(Double.MIN_VALUE + " to " + Double.MAX_VALUE);
+        
+
+//     }
+    
+// }
+
+
+
+
+// ==============================================================================================================
+ 
+// PERFORMING THE OPPERATION IN JAVA 
+
+
+// public class dtype{
+//     public static void main(String[] args)
+//     {
+//         int a=5,b=6;
+//         System.out.println(a+b);
+//     }
+// }
+
+//================================================================================================================
